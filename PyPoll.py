a8d9748820adbe4d68c167fb12a451c817076bc2
neerajain9 <neerajain@hotmail.com>
2021-08-20 20:10:06 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -27,6 +27,20 @@ file_to_load = os.path.join("Resources","election_results.csv")
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
 ##############################
+# Initialize the total vote counter
+total_votes = 0
+
+# Declare a list to capture the name of the candidates
+candidate_options = []
+
+# Declare a dictionary to capture the votes for the candidates
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     # Read the file object with the reader function.
@@ -34,7 +48,60 @@ with open(file_to_load) as election_data:
     
     # print the header row..(next skips the 1st row)
     headers = next(file_reader)
-    print(headers)   
+
+
+    for row in file_reader:
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # Add the candidate name to the candidate list.
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+ 
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0           
+        
+        candidate_votes[candidate_name] += 1
+ 
+    # Determine the percentage of votes for each candidate by looping through the counts.
+    # 1. Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        # 2. Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        # 3. Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        # 4. Print the candidate name and percentage of votes.
+        #print(f"{candidate_name}: received {votes} votes: {round((vote_percentage),1)}% of the total votes.")    
+        # print out each candidate's name, vote count, and percentage of
+        # votes to the terminal.
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent =
+            # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+    
+    #Print the winner...
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+    print(winning_candidate_summary)
+    
+    #print(f"{winning_candidate} won the campaign and received {winning_count} votes: {round((winning_percentage),1)}% of the total votes.")    
+    print(total_votes)
+    print(candidate_options)
+    print(candidate_votes)
     
 #     # Print each row in the CSV file.
 #     rowCount = 0
@@ -52,16 +119,16 @@ with open(file_to_load) as election_data:
 #
 
 ##############################
-# Method 1 (Preferred)
-with open(file_to_save, 'w') as txt_file:
-    # Write some data to the file.
-    txt_file.write("Counties in the election")  
-    txt_file.write("\n------------------------")  
+# # Method 1 (Preferred)
+# with open(file_to_save, 'w') as txt_file:
+#     # Write some data to the file.
+#     txt_file.write("Counties in the election")  
+#     txt_file.write("\n------------------------")  
     
-    # txt_file.write("Arapahoe, ")    
-    # txt_file.write("Denver, ")    
-    # txt_file.write("Jefferson")    
-    txt_file.write("\nArapahoe, \nDenver, \nJefferson")
+#     # txt_file.write("Arapahoe, ")    
+#     # txt_file.write("Denver, ")    
+#     # txt_file.write("Jefferson")    
+#     txt_file.write("\nArapahoe, \nDenver, \nJefferson")
 
 ###############################
 # Method 2
